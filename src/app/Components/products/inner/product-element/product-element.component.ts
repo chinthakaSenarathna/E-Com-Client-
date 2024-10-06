46af50cc8816bb0a5856b8062de3cf7c8859eb9a
chinthaka senarathna <chironprabath@gmail.com>
2024-10-06 15:48:24 +0530

    card is okay

---
+++ src/app/Components/products/inner/product-element/product-element.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-product-element',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './product-element.component.html',
+  styleUrl: './product-element.component.css'
+})
+export class ProductElementComponent {
+
+}
